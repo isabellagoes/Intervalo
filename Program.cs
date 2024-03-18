b3c605d070f709b1e4e944326d0301cc2b059a8f
isabellagoes <141466024+isabellagoes@users.noreply.github.com>
2024-03-18 18:31:02 +0000

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite um número a fim de encontrá-lo em determinado intervalo: ");
+double entrada = Convert.ToDouble(Console.ReadLine());
+
+if(entrada >= 0 && entrada <= 25) {
+    Console.WriteLine("\nIntervalo: [0,25]");
+}
+
+else if(entrada > 25 && entrada <= 50) {
+    Console.WriteLine("\nIntervalo: (25,50]");
+}
+
+else if(entrada > 50 && entrada <= 75) {
+    Console.WriteLine("\nIntervalo: [50,75]");
+}
+
+else if(entrada > 75 && entrada <= 100) {
+    Console.WriteLine("\nIntervalo: (75,100]");
+}
+
+else {
+    Console.WriteLine("Fora de intervalo");
+}
